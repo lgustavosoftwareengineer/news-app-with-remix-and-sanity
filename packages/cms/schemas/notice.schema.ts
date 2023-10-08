836d5b6a4065e6c967ebdab2283f771ec35f216c
Luiz Gustavo <gugamatias7@gmail.com>
2023-10-08 12:07:15 -0300

    feat(cms): add validation and new fields in notice schema

--- packages/cms/schemas/notice.schema.ts
+++ packages/cms/schemas/notice.schema.ts
@@ -6,30 +6,45 @@ export const noticeSchema = defineType({
   name: 'notice',
   type: 'document',
   title: 'Notícias',
+  icon: BiNews,
   preview: {
     select: {
       title: 'title',
+      media: 'previewImage',
     },
-    prepare({title}) {
+    prepare({title, media}) {
       return {
         title,
-        media: BiNews,
+        media,
       }
     },
   },
+  groups: [{name: 'preview', default: true}, {name: 'content'}],
   fields: [
     defineField({
       name: 'title',
       type: 'string',
+      validation: (rule) => rule.required().error('The title field is required'),
+      group: 'preview',
     }),
     defineField({
       name: 'subtitle',
       type: 'string',
+      validation: (rule) => rule.required().error('The subtitle field is required'),
+      group: 'preview',
     }),
+    defineField({
+      name: 'previewImage',
+      type: 'image',
+      group: 'preview',
+    }),
+
     defineField({
       name: 'content',
       type: 'array',
       of: [{type: 'block'}, {type: 'image'}],
+      validation: (rule) => rule.required().error('This content is important to news'),
+      group: 'content',
     }),
     defineField({
       name: 'slug',
@@ -39,17 +54,23 @@ export const noticeSchema = defineType({
       },
       hidden: ({document}) => !document?.title,
       readOnly: ({document}) => !document?._id.startsWith('drafts.'),
+      validation: (rule) => rule.warning('This field is based in title field'),
+      group: 'content',
     }),
     defineField({
       name: 'writtenBy',
       type: 'string',
       initialValue: (param, {currentUser}) => currentUser?.name ?? '',
       readOnly: ({value}) => Boolean(value),
+      validation: (rule) => rule.warning('This field is based in current logged user'),
+      group: 'content',
     }),
     defineField({
       name: 'location',
       type: 'reference',
       to: [{type: locationSchema.name}],
+      validation: (rule) => rule.required().error('Required field'),
+      group: 'content',
     }),
   ],
 })
