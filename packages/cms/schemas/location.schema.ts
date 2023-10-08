803fb6aad0ddaf1bd19618958d96f7e83683d49b
Luiz Gustavo <gugamatias7@gmail.com>
2023-10-08 12:14:12 -0300

    feat: add validation and change icon to location schema

--- packages/cms/schemas/location.schema.ts
+++ packages/cms/schemas/location.schema.ts
@@ -5,6 +5,7 @@ export const locationSchema = defineType({
   name: 'location',
   type: 'document',
   title: 'Localizações',
+  icon: BiSolidCity,
   preview: {
     select: {
       name: 'state',
@@ -20,10 +21,17 @@ export const locationSchema = defineType({
     defineField({
       name: 'state',
       type: 'string',
+      placeholder: 'Select the state',
       options: {
         list: ['Rio de Janeiro', 'São Paulo', 'Minas Gerais', 'Pernambuco'],
       },
+      validation: (rule) => rule.required().error('The state field is required'),
     }),
-    defineField({name: 'city', type: 'string'}),
+    defineField({
+      name: 'city',
+      type: 'string',
+      placeholder: 'Select the city',
+      validation: (rule) => rule.required().error('The city field is required'),
+    }),
   ],
 })
